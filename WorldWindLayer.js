1ba479367bfd7ae3a6f55c3aea3b6c723de03f42
kahkitng98 <48977097+kahkitng98@users.noreply.github.com>
2019-10-20 00:01:02 +0800

    Update WorldWindLayer.js

--- WorldWindLayer.js
+++ WorldWindLayer.js
@@ -1,32 +1,11 @@
+var wwd = new WorldWind.WorldWindow("canvasOne");
+
+wwd.addLayer(new WorldWind.BMNGOneImageLayer());
+wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+
 wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
-
-var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-wwd.addLayer(placemarkLayer);
-
-var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-
-placemarkAttributes.imageOffset = new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.3,
-    WorldWind.OFFSET_FRACTION, 0.0);
-
-placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.5,
-            WorldWind.OFFSET_FRACTION, 1.0);
-
-placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
-
-var position = new WorldWind.Position(55.0, -106.0, 100.0);
-var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
-
-placemark.label = "Placemark\n" +
-    "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
-    "Lon " + placemark.position.longitude.toPrecision(5).toString();
-placemark.alwaysOnTop = true;
-
-placemarkLayer.addRenderable(placemark);
 
 
 
@@ -42,11 +21,11 @@ placemarkLayer.addRenderable(placemark);
 
 var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
 //MODAL2_M_AER_OD
-var layerName = "MOD_LSTD_CLIM_M";
+
 function changeMap(choice){
     wwd.addLayer(new WorldWind.BMNGOneImageLayer());
     wwd.addLayer(new WorldWind.BMNGLandsatLayer());
-    layerName = choice;
+    var layerName = choice;
     window.alert(choice);
     var createLayer = function (xmlDom) {
     var wms = new WorldWind.WmsCapabilities(xmlDom);
@@ -65,33 +44,5 @@ var logError = function (jqXhr, text, exception) {
 $.get(serviceAddress).done(createLayer).fail(logError);
 }
 
-//var createLayer = function (xmlDom) {
-//    var wms = new WorldWind.WmsCapabilities(xmlDom);
-//    var wmsLayerCapabilities = wms.getNamedLayer(layerName);
-//    var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
-//    var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
-//    wwd.addLayer(wmsLayer);
-//};
-//
-//var logError = function (jqXhr, text, exception) {
-//    console.log("There was a failure retrieving the capabilities document: " +
-//        text +
-//    " exception: " + exception);
-//};
-//
-//$.get(serviceAddress).done(createLayer).fail(logError);
 
 
-
-// wwd.addEventListener("mousemove", function (event) {
-
-//     ... // potentially respond to the event
-
-//     if (eventHandled) {
-//         event.preventDefault();
-//     }
-// });
-
-// if (event.defaultPrevented) {
-//     return; // return without doing anything
-// }
