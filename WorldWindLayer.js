c4c71a3774340e45e26e681dc68da280e48dee3d
yshunl <44795757+yshunl@users.noreply.github.com>
2019-10-19 21:16:07 +0800

    Update WorldWindLayer.js

--- WorldWindLayer.js
+++ WorldWindLayer.js
@@ -6,27 +6,3 @@ wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
-
-var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-wwd.addLayer(placemarkLayer);
-
-
-var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
-
-var layerName = "MOD_LSTD_CLIM_M";
-
-var createLayer = function (xmlDom) {
-    var wms = new WorldWind.WmsCapabilities(xmlDom);
-    var wmsLayerCapabilities = wms.getNamedLayer(layerName);
-    var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
-    var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
-    wwd.addLayer(wmsLayer);
-};
-
-var logError = function (jqXhr, text, exception) {
-    console.log("There was a failure retrieving the capabilities document: " +
-        text +
-    " exception: " + exception);
-};
-
-$.get(serviceAddress).done(createLayer).fail(logError);
